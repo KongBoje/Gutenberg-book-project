043c18081cb9a1be2d2bc598cb2d45b06a80d8c0
Michael <Michael_veilis@hotmail.com>
2018-05-01 20:38:06 +0200

    this

---
+++ src/main/java/com/mycompany/gutenbergproject/connections/Neo4jConnection.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gutenbergproject.connections;
+
+/**
+ *
+ * @author Micha
+ */
+public class Neo4jConnection {
+    public static void main(String[] args) {
+        System.out.println("Cypher");
+    }
+}
