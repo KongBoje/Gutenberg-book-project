3699e4f14ff010add676d779dfc5c6b0bccd4a55
Michael <Michael_veilis@hotmail.com>
2018-05-13 16:51:38 +0200

    package change

--- src/test/java/TDDTests/databaseTDD.java
+++ src/test/java/TDDTests/databaseTDD.java
@@ -1,10 +1,11 @@
-package com.mycompany.gutenbergproject.connections;
+package TDDTests;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import com.mycompany.gutenbergproject.connections.MongoDBConnection;
 import org.junit.Before;
 import org.junit.Test;
 
