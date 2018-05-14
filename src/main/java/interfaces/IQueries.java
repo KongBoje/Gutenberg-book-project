428f1c25e62f175dfe5968cd160d1da98b35084a
= <mart_h@live.dk>
2018-05-14 14:55:43 +0200

    Added methods to interface

---
+++ src/main/java/interfaces/IQueries.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import java.util.List;
+
+/**
+ *
+ * @author OpieOP
+ */
+public interface IQueries {
+    
+    //Getting all books(names)
+    public List<String> getAllBooks();
+    
+    //Getting all cities
+    public List<String> getAllCities();
+    
+    //Get all cities mentioned in books by certain author
+    public List<String> getAuthorRelatedCities(String author);
+    
+    //Author and author's books based off of city
+    public List<String> getAuthorBooksFromCities(String city);
+    
+    //Cities from book
+    public List<String> getCitiesFromBook(String bookName);
+    
+    //  Get cities' longtitude / Latitude  
+    // public Collection?<float> getLongtitudeLatitudeFromCity(String city);    
+    
+    
+}
