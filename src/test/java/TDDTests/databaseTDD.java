c83cda2061e2926ec4f5b2c54633d1043c0a6280
= <mart_h@live.dk>
2018-05-16 09:46:32 +0200

    One test works

--- src/test/java/TDDTests/databaseTDD.java
+++ src/test/java/TDDTests/databaseTDD.java
@@ -5,7 +5,14 @@ package TDDTests;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
 import com.mycompany.gutenbergproject.connections.MongoDBConnection;
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,26 +22,57 @@ import org.junit.Test;
  */
 public class databaseTDD {
 
-    private MongoDBConnection mongo;
+    private MongoClient mongo;
 
     public databaseTDD() {
     }
 
     @Before
     public void setUp() {
-        mongo = new MongoDBConnection();
+        mongo = getMongoConnection();
         //MySQLConnection sql = null;
         //Neo4jConnection; 
     }
 
-    
     @Test
-    public void getAllBooks(){
-    
+    public void getAllBooksMongoTest() {
+        int bookCount;
+        MongoClient myMongo = getMongoConnection();
+        ArrayList<String> books = new ArrayList<>(); 
+        DB db = getMongoDB(myMongo);
+        DBCollection table = db.getCollection("books");
+        DBCursor cursor = table.find();
+        for (DBObject DBobject : cursor) {
+            books.add(DBobject.get("Title").toString());
+        }
+        assertEquals(books.size(), 10);
     }
     
     
+    public MongoClient getMongoConnection() {
+        if (mongo != null) {
+            return mongo;
+        } else {
+            mongo = new MongoClient("localhost", 27017);
+            return mongo;
+        }
+    }
     
+    public DB getMongoDB(MongoClient client) {
+        DB db = client.getDB("bookstore");
+        return db;
+    }
+
+    @Test
+    public void getAllBooksSQL() {
+
+    }
+
+    @Test
+    public void getAllBooksGraph() {
+
+    }
+
 //    //Match string with books and find equal or similar titles. 
 //    @Test
 //    public void getBooksByNameTest() {
