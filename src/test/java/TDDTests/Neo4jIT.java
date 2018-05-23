5f08a98f5532f8fbfaa387afdc2d1b15e6504632
Michael <Michael_veilis@hotmail.com>
2018-05-23 17:59:17 +0200

    neo4j query tests

--- src/test/java/TDDTests/Neo4jIT.java
+++ src/test/java/TDDTests/Neo4jIT.java
@@ -7,7 +7,11 @@ package TDDTests;
 
 import com.mycompany.gutenbergproject.connections.Neo4jConnection;
 import java.util.ArrayList;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.neo4j.driver.v1.Driver;
@@ -21,49 +25,140 @@ import org.neo4j.driver.v1.StatementResult;
  */
 public class Neo4jIT {
 
+//    @Ignore
     @Test
     @DisplayName("Test connection, by getting all books")
     public void getAllBooks() {
         Driver driver = Neo4jConnection.getConnection();
         ArrayList<String> books = new ArrayList<>();
-        
+        int expectedResult = 36597;
+
         Session session = driver.session();
-        String query = "MATCH (s:Book)" +
-                "RETURN s.title AS title";
-        
+        String query = "MATCH (s:Book)"
+                + "RETURN s.title AS title";
+
         StatementResult result = session.run(query);
-        
-        while (result.hasNext()){
+
+        while (result.hasNext()) {
             Record record = result.next();
             books.add(record.get("title").asString());
         }
-        assertEquals(books.size(), 10);
-        
-        session.close();
-        driver.close();
+        int actualResult = books.size();
+
+        assertThat(books.isEmpty(), is(false));
+        assertThat(books.get(1), equalTo("`Abdul-Bahás Tablet to Dr. Forel"));
+        assertEquals(expectedResult, actualResult);
     }
 
+//    @Ignore
     @Test
     @DisplayName("Returns all titles and authors from a book in a given city")
     public void getBooksFromCityIT() {
         Driver driver = Neo4jConnection.getConnection();
+        ArrayList<String> books = new ArrayList<>();
+        int expectedResult = 32169;
+        String name = "\"'London'\"";
+        String index = "Wylders Hand" + " " + "Le Fanu, Joseph Sheridan";
+
+        Session session = driver.session();
+        String query = "MATCH ({name:" + name + "})<-[:MENTIONS]-(book:Book)<-[:WRITTEN]-(author:Author)\n"
+                + "RETURN book.title AS title, author.name AS name";
+
+        StatementResult result = session.run(query);
+
+        while (result.hasNext()) {
+            Record record = result.next();
+            books.add(record.get("title").asString() + " " + record.get("name").asString());
+        }
+        int actualResult = books.size();
+
+//        System.out.println(books);
+        assertThat(books.isEmpty(), is(false));
+        assertThat(books.containsAll(books), is(true));
+        assertThat(books.get(7), equalTo(index));
+        assertEquals(expectedResult, actualResult);
     }
 
+//    @Ignore
     @Test
     @DisplayName("Returns all cities mentioned in a books title and plots them onto a map")
     public void getCityFromBookTitleIT() {
+        Driver driver = Neo4jConnection.getConnection();
+        ArrayList<String> books = new ArrayList<>();
+        int expectedResult = 28;
+        String title = "\"Pocahontas, A Poem\"";
+        String index = "'Bay'" + " " + 14.1837 + " " + 121.286;
 
+        Session session = driver.session();
+        String query = "MATCH ({title:" + title + "})-[:MENTIONS]->(city:City)\n"
+                + "RETURN city.name AS name, city.latitude AS latitude, city.longitude AS longitude";
+
+        StatementResult result = session.run(query);
+
+        while (result.hasNext()) {
+            Record record = result.next();
+            books.add(record.get("name").asString() + " " + record.get("latitude").asDouble() + " " + record.get("longitude").asDouble());
+        }
+        int actualResult = books.size();
+
+        assertThat(books.isEmpty(), is(false));
+        assertThat(books.containsAll(books), is(true));
+        assertThat(books.get(10), equalTo(index));
+        assertEquals(expectedResult, actualResult);
     }
 
+//    @Ignore
     @Test
     @DisplayName("Returns all books written by that author and plots mentioned cities in those books onto a map")
     public void getBooksByAuthorIT() {
+        Driver driver = Neo4jConnection.getConnection();
+        ArrayList<String> books = new ArrayList<>();
+        int expectedResult = 387;
+        String name = "\"Adams, Charles Francis\"";
+        String index = "\"Imperialism\" and \"The Tracks of Our Forefathers\"" + " " + "'Venezuela'" + " " + 21.7375 + " " + -78.7934;
 
+        Session session = driver.session();
+        String query = "MATCH ({name:" + name + "})-[:WRITTEN]->(book:Book)-[:MENTIONS]->(city:City)\n"
+                + "RETURN book.title AS title, city.name AS name, city.latitude AS latitude, city.longitude AS longitude";
+
+        StatementResult result = session.run(query);
+
+        while (result.hasNext()) {
+            Record record = result.next();
+            books.add(record.get("title").asString() + " " + record.get("name").asString() + " " + record.get("latitude").asDouble() + " " + record.get("longitude").asDouble());
+        }
+        int actualResult = books.size();
+
+        assertThat(books.isEmpty(), is(false));
+        assertThat(books.containsAll(books), is(true));
+        assertThat(books.get(2), equalTo(index));
+        assertEquals(expectedResult, actualResult);
     }
 
+    @Ignore
     @Test
     @DisplayName("Returns all books mentioning a city in vicinity of the given geolocation")
     public void getBooksByGeolocationIT() {
+        Driver driver = Neo4jConnection.getConnection();
+        ArrayList<String> books = new ArrayList<>();
+        int expectedResult = 1;
+        String title = "";
+        String index = "";
 
+        Session session = driver.session();
+        String query = "";
+
+        StatementResult result = session.run(query);
+
+        while (result.hasNext()) {
+            Record record = result.next();
+            books.add(record.get("").asString());
+        }
+        int actualResult = books.size();
+
+        assertThat(books.isEmpty(), is(false));
+        assertThat(books.containsAll(books), is(true));
+        assertThat(books.get(10), equalTo(index));
+        assertEquals(expectedResult, actualResult);
     }
 }
